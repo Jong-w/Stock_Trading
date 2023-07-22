cd34e2497ba587e4c10d0912d27f533452c2c598
Jong-Won Kim <kjw65144128@gmail.com>
2023-07-22 14:23:27 +0900

    commit

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -5,10 +5,11 @@ from gym import spaces
 import pandas as pd
 import numpy as np
 
-MAX_ACCOUNT_BALANCE = 2147483647
-MAX_NUM_SHARES = 2147483647
+
+MAX_ACCOUNT_BALANCE = 782243419
+MAX_NUM_SHARES = 782243419
 MAX_SHARE_PRICE = 5000
-MAX_STEPS = 20000
+MAX_STEPS = 200000
 
 INITIAL_ACCOUNT_BALANCE = 10000
 
@@ -28,31 +29,33 @@ class StockTradingEnv(gym.Env):
 
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(6, 6), dtype=np.float16)
+            low=0, high=1, shape=(7, 6), dtype=np.float16)
 
     def _next_observation(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
         frame = np.array([
-            self.df.loc[self.current_step: self.current_step +
-                        5, 'Open'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        5, 'High'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        5, 'Low'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        5, 'Close'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        5, 'Volume'].values / MAX_NUM_SHARES
+            self.df['Tic'][[self.current_step, self.current_step+2000, self.current_step+4000, self.current_step +
+                            6000, self.current_step+8000, self.current_step+10000]],
+            self.df['Open'][[self.current_step, self.current_step + 2000, self.current_step + 4000, self.current_step +
+                             6000, self.current_step + 8000, self.current_step + 10000]].values / max(self.df.Open),
+            self.df['High'][[self.current_step, self.current_step + 2000, self.current_step + 4000, self.current_step +
+                             6000, self.current_step + 8000, self.current_step+10000]].values / max(self.df.High),
+            self.df['Low'][[self.current_step, self.current_step + 2000, self.current_step + 4000, self.current_step +
+                            6000, self.current_step + 8000, self.current_step+10000]].values / max(self.df.Low),
+            self.df['Close'][[self.current_step, self.current_step + 2000, self.current_step + 4000, self.current_step +
+                              6000, self.current_step + 8000, self.current_step+10000]].values / max(self.df.Close),
+            self.df['Volume'][[self.current_step, self.current_step + 2000, self.current_step + 4000, self.current_step +
+                              6000, self.current_step + 8000, self.current_step+10000]].values / max(self.df.Volume)
         ])
 
         # Append additional data and scale each value to between 0-1
         # print(self.current_step)
         # print(frame.shape)
         obs1 = np.append(frame, [[
-            self.balance / MAX_ACCOUNT_BALANCE,
+            self.balance / MAX_ACCOUNT_BALANCE, #key1
             self.max_net_worth / MAX_ACCOUNT_BALANCE,
-            self.shares_held / MAX_NUM_SHARES,
             self.cost_basis / MAX_SHARE_PRICE,
+            self.shares_held / MAX_NUM_SHARES, # key2
             self.total_shares_sold / MAX_NUM_SHARES,
             self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
         ]], axis=0)
@@ -61,13 +64,12 @@ class StockTradingEnv(gym.Env):
 
     def _take_action(self, action):
         # Set the current price to a random price within the time step
-        current_price = random.uniform(
-            self.df.loc[self.current_step, "Open"], self.df.loc[self.current_step, "Close"])
+        current_price = random.uniform(self.df.loc[self.current_step, "Open"], self.df.loc[self.current_step, "Close"]) + 1e-7
 
         action_type = action[0]
-        amount = action[1]
+        amount = action[1] + 1e-7
 
-        if action_type < 1:
+        if action_type <= 1:
             # Buy amount % of balance in shares
             total_possible = int(self.balance / current_price)
             shares_bought = int(total_possible * amount)
@@ -75,11 +77,10 @@ class StockTradingEnv(gym.Env):
             additional_cost = shares_bought * current_price
 
             self.balance -= additional_cost
-            self.cost_basis = (
-                prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            self.cost_basis = (prev_cost + additional_cost) / (self.shares_held + shares_bought + 1e-7)
             self.shares_held += shares_bought
 
-        elif action_type < 2:
+        elif action_type <= 2:
             # Sell amount % of shares held
             shares_sold = int(self.shares_held * amount)
             self.balance += shares_sold * current_price
@@ -101,7 +102,7 @@ class StockTradingEnv(gym.Env):
 
         self.current_step += 1
 
-        if self.current_step > len(self.df.loc[:, 'Open'].values) - 6:
+        if self.current_step > len(self.df.loc[:, 'Open'].values) - 10001:
             self.current_step = 0
 
         delay_modifier = (self.current_step / MAX_STEPS)
@@ -124,8 +125,8 @@ class StockTradingEnv(gym.Env):
         self.total_sales_value = 0
 
         # Set the current step to a random point within the data frame
-        self.current_step = random.randint(
-            0, len(self.df.loc[:, 'Open'].values) - 6)
+        self.current_step = 0
+        #self.current_step = random.randint(0, len(self.df.loc[:, 'Open'].values) - 2000)
 
         return self._next_observation()
 
@@ -134,11 +135,8 @@ class StockTradingEnv(gym.Env):
         profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
 
         print(f'Step: {self.current_step}')
-        print(f'Balance: {self.balance}')
-        print(
-            f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
-        print(
-            f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
-        print(
-            f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+        #print(f'Balance: {self.balance}')
+        #print(f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
+        #print(f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+        print(f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
         print(f'Profit: {profit}')
--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import gym
-import json
-import datetime as dt
+#import gym
+#import json
+#import datetime as dt
 
 # from stable_baselines3.common.policies import MlpPolicy
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3 import PPO
-import talib as ta
+#import talib as ta
 from env.StockTradingEnv import StockTradingEnv
+import torch
 
 import pandas as pd
 
-df = pd.read_csv('./data/AAPL.csv')
+df = pd.read_csv('./data/train_stock.csv')
+df.Tic = df.Tic.astype('category').cat.codes
+df.Tic = (df.Tic - min(df.Tic)) / (max(df.Tic) - min(df.Tic))
+df[['Open', 'High', 'Low', "Close", 'Volume']] = df[['Open', 'High', 'Low', "Close", 'Volume']].astype('float64')
 df = df.sort_values('Date')
 df.dropna(inplace=True)
 df = df.sort_values('Date')
-df = df.reset_index()
+df = df.reset_index(drop=True)
 
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: StockTradingEnv(df)])
+#env = StockTradingEnv(df)
 
+torch.autograd.set_detect_anomaly(True)
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=20000)
+model.learn(total_timesteps=2000)
 
 obs = env.reset()
 for i in range(2000):
